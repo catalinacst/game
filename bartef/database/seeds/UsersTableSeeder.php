cbdd4a51184e52f6ca651748246ccd4da0b72b2f
Yefri <yefri.gaitan97@gmail.com>
2017-01-30 11:40:02 -0500

    Updated name of users in UsersTableSeeder

--- bartef/database/seeds/UsersTableSeeder.php
+++ bartef/database/seeds/UsersTableSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,13 +13,18 @@ class UsersTableSeeder extends Seeder
     {
       DB::table('users')->delete();
 
-      $faker = Faker::create();
-      foreach (range(1, 9) as $index) {
-        DB::table('users')->insert([
-            'name' => $faker->name,
-            'created_at' => new DateTime,
-            'updated_at' => new DateTime,
-        ]);
-      }
+      $users = array(
+        ['name' => 'Ana Alonso Lopez', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Miguel Fajardo Velasco', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Antonia Martínez García', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Rick Harrison', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Manuel Domínguez Aguilar', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'María Jimenéz Diáz', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Javier Chacón Avilés', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Pilar Sanz Vilas', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ['name' => 'Warren Buffett', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+      );
+      
+      DB::table('users')->insert($users);
     }
 }
