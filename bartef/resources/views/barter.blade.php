dc1597d4309b61089142c3f724112edea938e122
Yefri <yefri.gaitan97@gmail.com>
2017-02-16 22:57:33 -0500

    View barter 80%

--- bartef/resources/views/barter.blade.php
+++ bartef/resources/views/barter.blade.php
@@ -3,56 +3,107 @@
 @section('title', 'Barter')
 
 @section('content')
-<div class="section">
-  <div class="container">
-    <input type="hidden" id="idUser" value="{{$to->id}}">
-    <form action="/exchange/{{$to->id}}" id="form-exchange">
-      <div class="row">
-        <br><br>
-
-        <!-- Left -->
-        <div class="col s6">
-          @foreach($from->objects as $object)
-          <p>
-            <input type="checkbox" class="left" id="left{{$itl}}" name="left{{$itl}}" value="{{$object->id}}" />
-            <label for="left{{$itl++}}">{{$object->name}}, {{$object->value}}, {{$object->category->name}}</label>
-          </p>
-          @endforeach
+<!-- Exchange zone -->
+<div class = "section">
+  <!-- User id -->
+  <input type="hidden" id="idUser" value="{{$to->id}}">
+  <!-- Usernames and objects proposed -->
+  <div class="row">
+    <!--- User 1 -->
+    <div class="col s12 l6 m12">
+      <!-- Username -->
+      <h4 class="center">{{ $from->name }}</h4>
+      <!-- Objects proposed -->
+      <div class="#b0bec5 blue-grey lighten-3 inter01">
+        <div class="row">
+          <!-- foreach -->
+          <div class="col s2">
+            <div class="objects01 sub-inter"></div>
+          </div>
+          <!-- end foreach -->
         </div>
-
-        <!-- Right -->
-        <div class="col s6">
-          @foreach($to->objects as $object)
-            <div>
-              <p>
-                <input type="checkbox" class="right" id="right{{$itr}}" name="right{{$itr}}" value="{{$object->id}}" disabled/>
-                <label for="right{{$itr++}}">{{$object->name}}, {{$object->value}}, {{$object->category->name}}</label>
-              </p>
-            </div>
-          @endforeach
-        </div>
-
-        <br><br>
       </div>
-    </form>
-
-    <!-- Button Proponer -->
-    <div class="center">
-      <button class="btn waves-effect waves-light btn" type="submit" onclick="main()">Proponer
-        <i class="material-icons right">send</i>
-      </button>
     </div>
-
-    <br>
-
-    <!-- Button Get Started -->
-    <div class="center">
-      <button class="btn waves-effect waves-light orange btn-large" type="submit" form="form-exchange">Aceptar
+    <!--- User 2 -->
+    <div class="col s12 l6 m12">
+      <!-- Username -->
+      <h4 class="center">{{ $to->name }}</h4>
+      <!-- Objects proposed -->
+      <div class="#b0bec5 blue-grey lighten-3 inter01">
+        <div class="row">
+          <!-- foreach -->
+          <div class="col s2">
+            <div class="objects02 materialboxed sub-inter" data-caption="A picture"></div>
+          </div>
+          <!-- end foreach -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Buttons Regresar, Proponer or Aceptar -->
+  <div class = "center">
+      <!-- Back to Home -->
+      <a class="waves-effect waves-light btn" href="/home">Regresar</a>
+      <!-- Accept -->
+      <button class="btn-floating btn btn-large waves-effect waves-light orange" type="submit" form="form-exchange">
         <i class="material-icons right">done</i>
       </button>
+      <!-- Call main() -->
+      <a class="waves-effect waves-light btn" onclick="main()">Proponer</a>
     </div>
+</div>
 
-    <br><br>
+<!-- Display objects -->
+<div class="section">
+  <div class = "container">
+    <form action="/exchange/{{$to->id}}" id="form-exchange">
+      <div class="row">
+        <!-- My objects -->
+        <div class="col s6">
+          <!-- Collection my objects -->
+          <ul class="collection with-header">
+            <!-- Header Collection -->
+            <li class="collection-header"><h4>Mis cosas</h4></li>
+            <!-- Body Collection -->
+            @foreach($from->objects as $object)
+            <li class="collection-item avatar">
+              {!! Html::image('img/objects/'.$object->id.'.jpg', 'img', array('class' => 'circle')) !!}
+              <span class="title">{{$object->name}}</span>
+              <p>Categoria: {{$object->category->name}} <br>
+                 Valor aprox: ${{$object->value}}
+              </p>
+              <p class="secondary-content">
+                <input type="checkbox" class="left" id="left{{$itl}}" name="left{{$itl}}" value="{{$object->id}}" />
+                <label for="left{{$itl++}}"></label>
+              </p>
+            </li>
+            @endforeach
+          </ul>
+        </div>
+        <!-- Their objects -->
+        <div class="col s6">
+          <!-- Collection their objects -->
+          <ul class="collection with-header">
+            <!-- Header Collection -->
+            <li class="collection-header"><h4>Sus cosas</h4></li>
+            <!-- Body Collection -->
+            @foreach($to->objects as $object)
+            <li class="collection-item avatar">
+              {!! Html::image('img/objects/'.$object->id.'.jpg', 'img', array('class' => 'circle')) !!}
+              <span class="title">{{$object->name}}</span>
+              <p>Categoria: {{$object->category->name}} <br>
+                 Valor aprox: ${{$object->value}}
+              </p>
+              <p class="secondary-content">
+                <input type="checkbox" class="right" id="right{{$itr}}" name="right{{$itr}}" value="{{$object->id}}" disabled/>
+                <label for="right{{$itr++}}"></label>
+              </p>
+            </li>
+            @endforeach
+          </ul>
+        </div>
+      </div>
+    </form>
   </div>
 </div>
 @stop
