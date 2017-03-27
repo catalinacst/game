43b7772dddf3a4454a39aa0ecd5d2e63c124938c
Yefri <yefri.gaitan97@gmail.com>
2017-03-26 20:08:37 -0500

    Agregué metodo 'refresh' al controlador

--- bartef/app/Http/Controllers/AuthController.php
+++ bartef/app/Http/Controllers/AuthController.php
@@ -8,6 +8,8 @@ use App\Http\Requests;
 use Illuminate\Support\Facades\Hash;
 use Auth;
 use App\Status;
+use App\User;
+use App\Object;
 
 class AuthController extends Controller
 {
@@ -24,7 +26,7 @@ class AuthController extends Controller
      *
      */
     public function check(Request $request) {
-      if (Auth::attempt(['password' => $request['password']]))
+      if (Auth::attempt(['password' => $request['password']]) and Status::find('1')->flag == 1)
         return redirect('/welcome');
       return redirect('/login');
     }
@@ -63,10 +65,27 @@ class AuthController extends Controller
      *  Refresh: Deactivate the app
      *
     */
-    public function refresh(){
+    public function refresh() {
+      // Deactivate flag
       $state = Status::find('1');
       $state->flag = 0;
       $state->save();
+      // Refresh interests
+      $user = User::find('1');
+      $user->name = 'default';
+      $user->interests()->detach();
+      $user->save();
+      // Refresh objects
+      $array = array(1, 4, 4, 5, 4, 6, 5, 1, 4, 2, 6, 1, 2, 2, 5, 8, 4,
+                     3, 2, 6, 7, 5, 6, 3, 4, 6, 3, 2, 1, 3, 4, 7, 3, 6,
+                     7, 2, 1, 2, 5, 9, 1, 6, 6, 4, 6, 7, 5, 8, 5, 6, 4,
+                     6, 1, 7, 6, 8, 2, 2, 1, 5, 5, 6, 8, 9, 4, 1, 2, 5,
+                     7, 7, 1, 9);
+      foreach ($array as $key => $value) {
+        $obj = Object::find($key + 1);
+        $obj->user_id = $value;
+        $obj->save();
+      }
       return redirect('/generate');
     }
 }
