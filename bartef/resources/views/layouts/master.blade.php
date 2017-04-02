1fd13f332bde0a5716e67815625c0c55ccd5633f
Catalina Castro <catalina.cst@hotmail.es>
2017-04-02 03:07:00 -0500

    Se organizó el camino, centrando la flecha y container (el container queda sujeto a cambios)

--- bartef/resources/views/layouts/master.blade.php
+++ bartef/resources/views/layouts/master.blade.php
@@ -32,174 +32,184 @@
 <div id="path" class="modal bottom-sheet">
   <div class="modal-content  grey lighten-5">
     <center> <h3 class="Bold">Camino</h3> </center>
-    <div class="row">
-      <div class="col s6">
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/71.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/6.jpg') !!}">
-          </div>
-        </div>
+    <div class="container">
+      <div class="row">
+        <div class="col s6">
 
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/20.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/3.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/37.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/2.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/38.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/19.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/12.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/53.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/4.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/31.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/5.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/44.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/65.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/5.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/30.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/8.jpg') !!}">
-          </div>
-        </div>
-
-      </div>
-
-      <div class="col s6">
-
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/15.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/61.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/22.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/6.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/62.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/55.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/7.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/32.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/8.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br>
-        <div class="row">
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/56.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img materialboxed" src="{!! asset('img/objects/48.jpg') !!}">
-          </div>
-          <div class="col s2">
-            <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
-          </div>
-          <div class="col s2">
-            <img class="sizeimg responsive-img" src="{!! asset('img/users/9.jpg') !!}">
-          </div>
-        </div>
-
-        <div class="divider"></div> <br> <br>
-        <div class="row">
-          <div class="col s7">
-            <div class="modal-footer">
-              <a href="#!" class="modal-action modal-close waves-effect waves-green btn">Entendido!</a>
+          <div class="row">
+            <div class="col s2 offset-s4 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/71.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/6.jpg') !!}">
             </div>
           </div>
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 offset-s4 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/20.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/3.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 offset-s4 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/37.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/2.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br>
+
+          <div class="row">
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/38.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/19.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/12.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/4.jpg') !!}">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/53.jpg') !!}">
+            </div>
+          </div>
+
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/31.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/5.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/44.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/5.jpg') !!}">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/65.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/30.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/8.jpg') !!}">
+            </div>
+          </div>
+
         </div>
 
+        <div class="col s6">
+
+          <div class="row">
+            <div class="col s2 offset-s1 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/15.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/61.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/22.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/6.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 offset-s3 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/62.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/55.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/7.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 offset-s5 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/32.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/8.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br>
+          <div class="row">
+            <div class="col s2 offset-s3 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/56.jpg') !!}">
+            </div>
+            <div class="col s2 objects">
+              <img class="responsive-img materialboxed" src="{!! asset('img/objects/48.jpg') !!}">
+            </div>
+            <div class="col s2">
+              <img class="responsive-img" src="{!! asset('img/flecha.png') !!}">
+            </div>
+            <div class="col s3 users">
+              <img class="responsive-img" src="{!! asset('img/users/9.jpg') !!}">
+            </div>
+          </div>
+
+          <hr class="style1"> <br> <br>
+          <div class="row">
+            <div class="col s9">
+              <div class="modal-footer">
+                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-large blue">Entendido!</a>
+              </div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
+
   </div>
 </div>
 @stop
